3c5c1e67c27b68ec73304ed80388d91fe516c1ea
César <cesardsbraz@gmail.com>
2025-04-23 15:57:03 -0300

    add tela de login

--- front/poop-ranking/src/pages/Login.jsx
+++ front/poop-ranking/src/pages/Login.jsx
@@ -1,31 +1,16 @@
 import React from 'react'
 
-function Register() {
-    return (
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+function Login() {
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                 <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-                    cadastre-se!
+                    entre!
                 </h2>
             </div>
 
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                 <form action="#" method="POST" className="space-y-6">
-                    <div>
-                        <label htmlFor="name" className="block text-sm/6 font-medium text-gray-900">
-                            nome
-                        </label>
-                        <div className="mt-2">
-                            <input
-                                id="name"
-                                name="name"
-                                type="text"
-                                required
-                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                            />
-                        </div>
-                    </div>
-
                     <div>
                         <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
                             email
@@ -36,6 +21,7 @@ function Register() {
                                 name="email"
                                 type="email"
                                 required
+                                autoComplete="email"
                                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                             />
                         </div>
@@ -51,6 +37,7 @@ function Register() {
                                 name="password"
                                 type="password"
                                 required
+                                autoComplete="current-password"
                                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                             />
                         </div>
@@ -61,20 +48,20 @@ function Register() {
                             type="submit"
                             className="cursor-pointer flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
-                            registrar
+                            entrar
                         </button>
                     </div>
                 </form>
 
                 <p className="mt-10 text-center text-sm/6 text-gray-500">
-                    já tem uma conta?{' '}
+                    ainda não tem uma conta?{' '}
                     <a href="#" className="cursor-pointer font-semibold text-indigo-600 hover:text-indigo-500">
-                        faça login aqui
+                        cadastre-se aqui
                     </a>
                 </p>
             </div>
         </div>
-    )
+  )
 }
 
-export default Register
+export default Login
