c1df29deb2b6201b156d66d6ce171d122017fd66
César <cesardsbraz@gmail.com>
2025-04-23 14:57:08 -0300

    add tela de registro

--- front/poop-ranking/src/App.jsx
+++ front/poop-ranking/src/App.jsx
@@ -1,8 +1,10 @@
+import Register from "./pages/Register"
+
 function App() {
   return (
-    <>
-      <h1 className='text-4xl'>poop</h1>
-    </>
+    <main className="w-screen h-screen flex items-center justify-center bg-gradient-to-b from-[#f5f5f5] to-[#e0e0e0]">
+      <Register />
+    </main>
   )
 }
 
